6c03ec8e7fd95fddf8a8562719706ab79f3b7c22
emre kaya <emrekaya77@gmail.com>
2021-02-22 11:20:36 +0300

    23 Ocak ders sonuu

---
+++ OOP2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IndividualCustomer customer1 = new IndividualCustomer();
+            customer1.Id = 1;
+            customer1.CustomerNo = "12345";
+            customer1.TcNo = "27493064090";
+            customer1.Name = "Kadir";
+            customer1.FirstName = "Kaya";
+
+
+            CoorporateCustomer customer2 = new CoorporateCustomer();
+            customer2.Id = 2;
+            customer2.CustomerNo = "54321";
+            customer2.CompanyName = "Kodlama.ıo";
+            customer2.TaxNo = "9876543210";
+
+
+            Customer customer3 = new IndividualCustomer();      // Burda dikkat edersek müsteri gercek müsteriyi de tutabiliyor tüzel müşteriyi de tutabiliyor
+            Customer customer4 = new CoorporateCustomer();      // Yani müsteri classı hem gercek müsterinin referansını tutabiliyor hem de tüzel müşterinin
+
+            CustomerManeger customerManeger = new CustomerManeger();
+            customerManeger.Add(customer1);
+            customerManeger.Add(customer2);
+            customerManeger.Add(customer3);
+            customerManeger.Add(customer4);
+
+
+                
+                 
+
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            konutKrediManager.Hesapla();
+
+            IKrediManager ıhtiyacKrediManager = new IhtiyacKrediManager();
+            ıhtiyacKrediManager.Hesapla();
+
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            tasitKrediManager.Hesapla();
+
+            IKrediManager esnafKrediManager = new EsnafKredisiManager();
+            esnafKrediManager.Hesapla();
+            //yani yukarıdan burdan anlıyoruz ki Interface onu implemente(uygulayan) eden classın referansını tutabilir.
+
+            Console.WriteLine("-------------------");
+
+            ILoggerService databaseLoggerService = new DataBaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+            ILoggerService smsLoggerServive = new SmsLoggerService();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(ıhtiyacKrediManager, databaseLoggerService, smsLoggerServive);
+            basvuruManager.BasvuruYap(konutKrediManager, fileLoggerService , smsLoggerServive);
+            basvuruManager.BasvuruYap(tasitKrediManager, databaseLoggerService, smsLoggerServive);   
+            basvuruManager.BasvuruYap(esnafKrediManager, fileLoggerService , smsLoggerServive );
+
+
+            //bu kısım işimizi kolaylaştırır başvuru manager class'ı açıp içerisine IKrediManager Interfacesini yazdık
+            //böyelelikle başvuru yapacağımız zaman kredileri tek tek yazmamıza gerek yoktur yukarıdaki gibi 
+            //BasvuruYap diyerek hangi krediyi istiyorsak oraya başvurabiliriz.
+
+            //bu kısım mülakatlarda karşımıza çıkar çok önemli ve ayırıcı bir bilgidir.
+            Console.WriteLine("--------------");
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ıhtiyacKrediManager, konutKrediManager, tasitKrediManager };
+            basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+        }
+    }
+}
